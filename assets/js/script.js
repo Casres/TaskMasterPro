8de0a326ab36aff95934f7359a69889fc3146fa1
Casres <christian@casillas.tech>
2022-03-27 20:30:56 -0400

    AskBCS save point

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,6 +166,40 @@ $(".list-group").on("blur", "input[type='text']", function() {
     $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    $(this).children().each(function() {
+      var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+    var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+    console.log(text, date);
+    });
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
